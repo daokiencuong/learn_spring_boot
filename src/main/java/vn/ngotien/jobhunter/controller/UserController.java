de6986a72ee4dfa6ac9d7a3fa0007b8c19ba4b53
daokiencuong <daokiencuong04@gmail.com>
2025-03-26 21:29:49 +0700

    Add Getmapping and Postmapping

---
+++ src/main/java/vn/ngotien/jobhunter/controller/UserController.java
@@ -0,0 +1,29 @@
+package vn.ngotien.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.ngotien.jobhunter.domain.User;
+import vn.ngotien.jobhunter.service.UserService;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    // @GetMapping("/user/create")
+    @PostMapping("/user/create")
+    public User createNewUser(@RequestBody User postManUser) {
+
+        User newUser = this.userService.handleCreateUser(postManUser);
+
+        return newUser;
+    }
+
+}
