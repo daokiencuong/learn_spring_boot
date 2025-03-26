ba50a905d8625f6e20a15d8903b00e12be5ffd91
daokiencuong <daokiencuong04@gmail.com>
2025-03-26 21:58:40 +0700

    Add DeleteMapping

--- src/main/java/vn/ngotien/jobhunter/controller/UserController.java
+++ src/main/java/vn/ngotien/jobhunter/controller/UserController.java
@@ -1,6 +1,8 @@
 package vn.ngotien.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +12,25 @@ import vn.ngotien.jobhunter.service.UserService;
 
 @RestController
 public class UserController {
+
+    private final HelloController helloController;
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, HelloController helloController) {
         this.userService = userService;
+        this.helloController = helloController;
     }
 
-
-    // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User postManUser) {
-
         User newUser = this.userService.handleCreateUser(postManUser);
-
         return newUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id){
+        this.userService.handleDeleteUser(id);
+        return "Delete user";
     }
 
 }
