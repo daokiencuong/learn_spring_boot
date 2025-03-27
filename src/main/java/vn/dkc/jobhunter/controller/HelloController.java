8a66fad0f13388d6d3a0683d9b99ba74ef08b72f
daokiencuong <daokiencuong04@gmail.com>
2025-03-27 23:10:51 +0700

    JWT hashpass and SecurityFilter VD48

--- src/main/java/vn/dkc/jobhunter/controller/HelloController.java
+++ src/main/java/vn/dkc/jobhunter/controller/HelloController.java
@@ -3,13 +3,11 @@ package vn.dkc.jobhunter.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import vn.dkc.jobhunter.service.error.IdInvalidException;
-
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String getHelloWorld() {
-        return "Hello World (Hỏi Dân IT & Eric)";
+        return "hello world";
     }
 }
--- src/main/java/vn/dkc/jobhunter/controller/UserController.java
+++ src/main/java/vn/dkc/jobhunter/controller/UserController.java
@@ -3,10 +3,9 @@ package vn.dkc.jobhunter.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +22,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService, HelloController helloController) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
+        String hashPassword = this.passwordEncoder.encode(postManUser.getPassword());
+        postManUser.setPassword(hashPassword);
         User newUser = this.userService.handleCreateUser(postManUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
