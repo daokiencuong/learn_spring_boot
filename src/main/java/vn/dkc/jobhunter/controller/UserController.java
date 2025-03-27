0d6bbd631912c0cb8ed1b6910a6dca148e7b5f4a
daokiencuong <daokiencuong04@gmail.com>
2025-03-27 08:02:53 +0700

    Handle exception and use @ExceptionHandler

--- src/main/java/vn/dkc/jobhunter/controller/UserController.java
+++ src/main/java/vn/dkc/jobhunter/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.dkc.jobhunter.domain.User;
 import vn.dkc.jobhunter.service.UserService;
+import vn.dkc.jobhunter.service.error.IdInvalidException;
 
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -47,8 +49,16 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.handleUpdateUser(user));
     }
 
+    @ExceptionHandler(IdInvalidException.class)
+    public ResponseEntity<String> handleIdInvalidException(IdInvalidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") long id){
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") long id) throws IdInvalidException{
+        if(id >= 100){
+            throw new IdInvalidException("Id is invalid");
+        }
+
         this.userService.handleDeleteUser(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
---
+++ src/main/java/vn/dkc/jobhunter/service/error/IdInvalidException.java
@@ -0,0 +1,8 @@
+package vn.dkc.jobhunter.service.error;
+
+public class IdInvalidException extends Exception{
+    //Constructor for IdInvalidException
+    public IdInvalidException(String message) {
+        super(message);
+    }
+}
